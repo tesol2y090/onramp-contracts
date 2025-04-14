2ecbbdf43597b1305dca71dc8b5d990174dbc778
tesol2y090 <gliese090@gmail.com>
2025-04-14 22:48:25 +0700

    feat: make axelar executable upgradable

---
+++ contracts/axelar/AxelarExecutable.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {IAxelarGateway} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGateway.sol";
+import {IAxelarExecutable} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarExecutable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+/**
+ * @title AxelarExecutable
+ * @dev Abstract contract to be inherited by contracts that need to execute cross-chain commands via Axelar's Gateway.
+ * It implements the IAxelarExecutable interface and is upgradeable.
+ */
+abstract contract AxelarExecutable is IAxelarExecutable, Initializable {
+    /// @dev Reference to the Axelar Gateway contract.
+    address internal gatewayAddress;
+
+    /**
+     * @dev Contract constructor that disables initializers.
+     */
+    constructor() {
+        _disableInitializers();
+    }
+
+    /**
+     * @dev Initializes the contract with the Axelar Gateway address.
+     * Reverts if the provided address is the zero address.
+     * @param gateway_ The address of the Axelar Gateway contract.
+     */
+    function __AxelarExecutable_init(
+        address gateway_
+    ) internal onlyInitializing {
+        if (gateway_ == address(0)) revert InvalidAddress();
+        gatewayAddress = gateway_;
+    }
+
+    /**
+     * @notice Executes the cross-chain command after validating it with the Axelar Gateway.
+     * @dev This function ensures the call is approved by Axelar Gateway before execution.
+     * It uses a hash of the payload for validation and internally calls _execute for the actual command execution.
+     * Reverts if the validation fails.
+     * @param commandId The unique identifier of the cross-chain message being executed.
+     * @param sourceChain The name of the source chain from which the message originated.
+     * @param sourceAddress The address on the source chain that sent the message.
+     * @param payload The payload of the message payload.
+     */
+    function execute(
+        bytes32 commandId,
+        string calldata sourceChain,
+        string calldata sourceAddress,
+        bytes calldata payload
+    ) external virtual {
+        bytes32 payloadHash = keccak256(payload);
+
+        if (
+            !gateway().validateContractCall(
+                commandId,
+                sourceChain,
+                sourceAddress,
+                payloadHash
+            )
+        ) revert NotApprovedByGateway();
+
+        _execute(commandId, sourceChain, sourceAddress, payload);
+    }
+
+    /**
+     * @dev Internal virtual function to be overridden by child contracts to execute the command.
+     * It allows child contracts to define their custom command execution logic.
+     * @param commandId The identifier of the command to execute.
+     * @param sourceChain The name of the source chain from which the command originated.
+     * @param sourceAddress The address on the source chain that sent the command.
+     * @param payload The payload of the command to be executed.
+     */
+    function _execute(
+        bytes32 commandId,
+        string calldata sourceChain,
+        string calldata sourceAddress,
+        bytes calldata payload
+    ) internal virtual;
+
+    /**
+     * @notice Returns the address of the AxelarGateway contract.
+     * @return The Axelar Gateway instance.
+     */
+    function gateway() public view returns (IAxelarGateway) {
+        return IAxelarGateway(gatewayAddress);
+    }
+}
